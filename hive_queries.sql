e1edca2f8fa0f9e0010c43817bf8c2c68de7f05f
GYEONGSIK03 <maria_dev@sandbox-hdp.hortonworks.com>
2026-06-02 03:45:56 +0000

    Add station direction analysis query

--- hive_queries.sql
+++ hive_queries.sql
@@ -23,3 +23,13 @@ FROM mju_bus
 GROUP BY route_id
 ORDER BY total_ride DESC
 LIMIT 10;
+
+-- Station Direction Analysis
+-- Compare boarding and alighting passengers by station
+
+SELECT
+    station_ars_id,
+    SUM(ride_passenger) AS ride,
+    SUM(alight_passenger) AS alight
+FROM mju_bus
+GROUP BY station_ars_id;
